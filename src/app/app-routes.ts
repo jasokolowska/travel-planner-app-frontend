565c2863984cf3ea17b05969b2f5a39dd5c1926a
jasokolowska <joanna_s@op.pl>
2022-09-12 08:57:20 +0200

    routing modification

--- src/app/app-routes.ts
+++ src/app/app-routes.ts
@@ -2,5 +2,5 @@ import { Routes } from "@angular/router";
 import { SearchResultsComponent } from "./search/search-results/search-results.component";
 
 export const appRoutes: Routes = [
-    {path: 'search', component: SearchResultsComponent}
+    {path: 'search/:destination&:origin', component: SearchResultsComponent}
 ]
